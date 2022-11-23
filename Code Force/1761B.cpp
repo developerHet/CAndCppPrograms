3972c2d6e0309123c8164a0cd4166277203dff1c
developerHet <hetpatel2312.hp@gmail.com>
2022-11-23 18:52:56 +0530

    New added

---
+++ Code Force/1761B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << " = " << x << " , " << #y << " = " << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+
+
+typedef pair<int, int>  pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+ll MOD=1000000007;
+ 
+ 
+ 
+void solve() {
+    int n;
+    cin >> n;
+    set<int> st;
+    for(int i=0;i<n;i++) {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+    if(st.size()==2) cout << n/2+1 << endl;
+    else cout << n << endl;
+}
+ 
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+       solve();
+    }
+    return 0;
+}
