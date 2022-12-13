cb6a86cad2769b24b9a4e4245f5931cd480a28a5
developerHet <hetpatel2312.hp@gmail.com>
2022-12-13 09:28:49 +0530

    new program added

---
+++ Code Force/1766B.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << " = " << x << " , " << #y << " = " << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+
+
+typedef pair<int, int>  pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+ll MOD=1000000007;
+ 
+ 
+ 
+void solve() {
+    int n;
+    cin >> n;
+    map<string,int> mp;
+    string s;
+    cin >> s;
+    for(int i=0;i<n-1;i++) {
+        string curr="";
+        curr+=s[i];
+        curr+=s[i+1];
+        if(mp.find(curr)==mp.end()) {
+            mp[curr]=i;
+        } else {
+            if(mp[curr]!=i-1) {
+                cout << "YES" << endl;
+                return;
+            }
+        }
+    } 
+
+    
+    cout << "NO" << endl;
+}
+ 
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+       solve();
+    }
+    return 0;
+}
