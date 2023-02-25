fa3b0481cccdc9979c296de7dbb2d8e15f825b23
developerHet <hetpatel2312.hp@gmail.com>
2023-02-25 18:46:35 +0530

    New program is added

---
+++ Code Force/1303B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << " = " << x << " , " << #y << " = " << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+
+
+typedef pair<int, int>  pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+
+int MOD=1000000007;
+ 
+ 
+ 
+void solve() {
+    int n,g,b;
+    cin >> n >>g >> b;
+    int z = (n+1)/2;
+    int x=(z/g)*(g+b);
+    if(z%g==0) {
+        x-=b;
+    } else {
+        x+=(z%g);
+    }
+    cout << max(n,x) << endl;
+}
+ 
+ 
+ 
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    cin.tie(NULL);
+    int t=1;
+    cin >> t;
+    while(t--) {
+       solve();
+    }
+    return 0;
+}
---
+++ Code Force/1312C.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << " = " << x << " , " << #y << " = " << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+
+
+typedef pair<int, int>  pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+
+int MOD=1000000007;
+ 
+ 
+ 
+void solve() {
+    int n,k;cin>>n>>k;
+    vi c(60);
+    for(int i=0;i<n;i++) {
+        int x;
+        cin >> x;
+        int j=0;
+        while(x) {
+            c[j]+=x%k;
+            x/=k;
+            j++;
+        }
+    }
+    for(int i=0;i<60;i++) {
+        if(c[i]>1) {
+            cout << "NO" << endl;
+            return;
+        } 
+    }
+    cout << "YES" << endl;
+}
+ 
+ 
+ 
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    cin.tie(NULL);
+    int t=1;
+    cin >> t;
+    while(t--) {
+       solve();
+    }
+    return 0;
+}
