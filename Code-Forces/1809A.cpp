9f34fe8935c691dc871609b8fe764072eb4ad3da
developerHet <hetpatel2312.hp@gmail.com>
2023-03-21 11:11:39 +0530

    New program added

--- Code-Forces/1809A.cpp
+++ Code-Forces/1809A.cpp
@@ -20,33 +20,10 @@ typedef vector<vi> vvi;
 
 int MOD=1000000007;
  
-int n,m,sx,sy,ex,ey;
-
-
-int check(int &x,int &y) {
-    bool f = 0;
-    if((x==-1 && sx==1) || (x==1 && sx==n))x*=-1,f=1;
-    if((y==-1 && sy==1) || (y==1 && sy==m))y*=-1,f=1;
-    return f;
-} 
-
+ 
+ 
 void solve() {
-    cin >>n>>m>>sx>>sy>>ex>>ey;
-    string s;cin>>s;
-    int x=1,y=1;
-    if(s[0]=='U') x=-1;
-    if(s[1]=='L') y=-1;
-    bool f = 0;
-    int bounce=0;
-    for(int i=0;i<4*n*m;i++) {
-        if(sx==ex && sy==ey) {
-            f=1;break;
-        }
-        bounce+=check(x,y);
-        sx+=x;sy+=y;
-    }
-    if(f) cout << bounce << endl;
-    else cout << -1 << endl; 
+    int n;
 }
  
  
